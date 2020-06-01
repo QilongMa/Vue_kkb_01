7c7c9512c36b10a6bdbf1371b65f8da084d9df1e
Will <maljean2012@gmail.com>
2020-05-31 21:20:58 -0400

    vue form

---
+++ src/components/form/KInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <input type="type" :value="value" @input="onInput">
+  </div>
+</template>
+
+<script>
+import emitter from "@/mixins/emitter";
+
+  export default {
+    inheritAttrs: false, //
+    mixins: [emitter],
+    props: {
+      value: {
+        type: String,
+        default: ''
+      },
+      type: {
+        type: String,
+        default: 'text'
+      }
+    },
+    methods: {
+      onInput(e) {
+        this.$emit('input', e.target.value);
+        this.dispatch('KFormItem', "validate");
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
